95682edf6252997d2d2e6848fcaa13a7832909e8
cheznewa <chezsick@orange.fr>
2022-11-15 12:46:36 +0100

    update

--- proc.php
+++ proc.php
@@ -8,17 +8,17 @@ echo "I'm Sorry";
 }
 $country = $_POST["country"];
 $mode = $_POST["mode"];
-exec("ffmpeg -i ".$tmp."/video -vf framerate=24,scale=720x576 ".$tmp."/%d.png ".$tmp."/a.wav");
+exec("ffmpeg -i ".$tmp."/video -vf framerate=".$_POST["framerate"].",scale=720x576 ".$tmp."/%d.png ".$tmp."/a.wav");
 $frame = count(scandir($tmp))-2;
 exec("python3 procpy.py ".$frame." ".$tmp." ".$country." ".$mode);
 
 if ("multi" == $mode)
 {
-exec("ffmpeg -r 24 -i ".$tmp."/%d_b.png -r 24 -i ".$tmp."/%d_a.png -i ".$tmp."/a.wav -vcodec libx264 -acodec copy -map 0 -map 1 -map 2 -f avi ".$tmp."/videodone.avi");
+exec("ffmpeg -r ".$_POST["framerate"]." -i ".$tmp."/%d_b.png -r 24 -i ".$tmp."/%d_a.png -i ".$tmp."/a.wav -vcodec png -acodec copy -map 0 -map 1 -map 2 -f avi ".$tmp."/videodone.avi");
 }
 else
 {
-exec("ffmpeg -r 24 -i ".$tmp."/%d_a.png -i ".$tmp."/a.wav -vcodec libx264 -f avi ".$tmp."/videodone.avi");
+exec("ffmpeg -r ".$_POST["framerate"]." -i ".$tmp."/%d_a.png -i ".$tmp."/a.wav -vcodec png -f avi ".$tmp."/videodone.avi");
 }
 header("Content-Description: File Transfer"); 
 header("Content-Type: application/octet-stream"); 
